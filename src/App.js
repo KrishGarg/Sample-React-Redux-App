fb53f202181eaaee2b2bb88f83d4d91a6fde9139
Krish Garg <krishgarg6306@gmail.com>
2021-08-30 03:24:40 +0530

    added github link button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
   set,
 } from "./slices/counterSlice";
 
+// Github Logo
+import ghLogo from './icons/GitHub-Mark-Light-64px.png';
+
 export const App = ({
   counter,
   increment,
@@ -21,92 +24,103 @@ export const App = ({
   const setRef = useRef();
 
   return (
-    <div>
-      <h1>Control the Counter</h1>
-      <button
-        className="main-btn"
-        onClick={() => {
-          increment();
-        }}
-      >
-        Click Me To Increment By 1
-      </button>
-      <button
-        className="main-btn"
-        onClick={() => {
-          decrement();
-        }}
-      >
-        Click Me To Decrement By 1
-      </button>
-      <br />
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const val = parseInt(incRef.current.value);
-          incrementBy(val);
-          incRef.current.value = null;
-        }}
-      >
-        <input
-          ref={incRef}
-          type="number"
-          name="inc"
-          id="inc"
-          placeholder="Increment By"
-        />
-        <button className="extra-btn" type="submit">
-          Enter
+    <>
+      <div>
+        <h1>Control the Counter</h1>
+        <button
+          className="main-btn"
+          onClick={() => {
+            increment();
+          }}
+        >
+          Click Me To Increment By 1
         </button>
-      </form>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const val = parseInt(decRef.current.value);
-          decrementBy(val);
-          decRef.current.value = null;
-        }}
-      >
-        <input
-          ref={decRef}
-          type="number"
-          name="inc"
-          id="inc"
-          placeholder="Decrement By"
-        />
-        <button className="extra-btn" type="submit">
-          Enter
+        <button
+          className="main-btn"
+          onClick={() => {
+            decrement();
+          }}
+        >
+          Click Me To Decrement By 1
         </button>
-      </form>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const val = parseInt(setRef.current.value);
-          set(val);
-          setRef.current.value = null;
-        }}
-      >
-        <input
-          ref={setRef}
-          type="number"
-          name="inc"
-          id="inc"
-          placeholder="Set to"
-        />
-        <button className="extra-btn" type="submit">
-          Enter
+        <br />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const val = parseInt(incRef.current.value);
+            incrementBy(val);
+            incRef.current.value = null;
+          }}
+        >
+          <input
+            ref={incRef}
+            type="number"
+            name="inc"
+            id="inc"
+            placeholder="Increment By"
+          />
+          <button className="extra-btn" type="submit">
+            Enter
+          </button>
+        </form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const val = parseInt(decRef.current.value);
+            decrementBy(val);
+            decRef.current.value = null;
+          }}
+        >
+          <input
+            ref={decRef}
+            type="number"
+            name="inc"
+            id="inc"
+            placeholder="Decrement By"
+          />
+          <button className="extra-btn" type="submit">
+            Enter
+          </button>
+        </form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const val = parseInt(setRef.current.value);
+            set(val);
+            setRef.current.value = null;
+          }}
+        >
+          <input
+            ref={setRef}
+            type="number"
+            name="inc"
+            id="inc"
+            placeholder="Set to"
+          />
+          <button className="extra-btn" type="submit">
+            Enter
+          </button>
+        </form>
+        <button
+          className="main-btn"
+          onClick={() => {
+            set(0);
+          }}
+        >
+          Reset Back to 0
         </button>
-      </form>
+        <h2>Counter: {counter}</h2>
+      </div>
       <button
-        className="main-btn"
         onClick={() => {
-          set(0);
+          window.location.href =
+            "https://github.com/KrishGarg/Sample-React-Redux-App";
         }}
+        className='gh-btn'
       >
-        Reset Back to 0
+        <img src={ghLogo} alt="Github" />
       </button>
-      <h2>Counter: {counter}</h2>
-    </div>
+    </>
   );
 };
 
